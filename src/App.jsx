341e99bfa82563d1df14ce191887857b1f1482a2
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-12-26 21:03:17 +0530

    Bringing Tailwind Css o the Game

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,30 @@ import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
+    <div className="flex h-full">
       <CssBaseline />
       <Navbar />
-      <Routes>
-        <Route path="/" exact element={<h1>Movies Data</h1>} />
-        <Route
-          path="/movie/:id"
-          exact
-          element={<h1>Particular Movie Data</h1>}
-        />
-        <Route
-          path="/actors/:id"
-          exact
-          element={<h1>Particular Actors Information</h1>}
-        />
-        <Route
-          path="/profile/:id"
-          exact
-          element={<h1>Profile Information</h1>}
-        />
-      </Routes>
+      <main className="p-8 grow">
+        <div className="h-16" />
+        <Routes>
+          <Route path="/" exact element={<h1>Movies Data</h1>} />
+          <Route
+            path="/movie/:id"
+            exact
+            element={<h1>Particular Movie Data</h1>}
+          />
+          <Route
+            path="/actors/:id"
+            exact
+            element={<h1>Particular Actors Information</h1>}
+          />
+          <Route
+            path="/profile/:id"
+            exact
+            element={<h1>Profile Information</h1>}
+          />
+        </Routes>
+      </main>
     </div>
   );
 };
